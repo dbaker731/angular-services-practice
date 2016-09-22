76cb1319b04f713819fd283ba249a91a3c796b70
dbaker731 <dbaker731@gmail.com>
2016-09-22 12:47:22 -0500

    Create ctrl.js

---
+++ ctrl.js
@@ -0,0 +1,19 @@
+angular.module( 'myApp' ).controller( 'myCtrl', function( $scope, niceService ){
+
+  $scope.niceList = niceService.niceList;
+  $scope.naughtyList = niceService.naughtyList;
+
+  $scope.addNewPerson = function(){
+    var person = buildPerson();
+    niceService.addPerson( person );
+  };
+
+  function buildPerson(){
+    return{
+      name: $scope.name,
+      niceScore: $scope.niceScore,
+      naughtyScore: $scope.naughtyScore
+    };
+  }
+
+});
